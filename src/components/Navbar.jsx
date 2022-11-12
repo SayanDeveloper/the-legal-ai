88091e00f7df7fb3f1ba4960ee6c2821520d3718
Sayan Samanta <samantasayan23@gmail.com>
2022-11-12 23:04:19 +0530

    asset setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from "../assets/logo.png"
+
+const Navbar = () => {
+    return (
+        <div>
+            <img src={logo} />
+            
+        </div>
+    )
+}
+
+export default Navbar
