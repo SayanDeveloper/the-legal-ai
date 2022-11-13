4a9fa26917ca357ca7723f5d08abeca07bd579b2
Sayan Samanta <samantasayan23@gmail.com>
2022-11-13 22:07:28 +0530

    added banner

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,22 @@
 import React from 'react'
 import "../styles/main.css"
-import banner from "../assets/banner.webp"
+import banner from "../assets/s2-1.svg"
 
 const Home = () => {
     return (
         <div className='home'>
             <div className='landing-banner'>
-                <img src={banner} alt="banner" />
                 <div>
-                    <h1>WE GIVE <br/>YOU RIGHT.</h1>
-                    <button>Get Assessment</button>
+                    <h2>VERKAUFE DEIN PROBLEM!</h2>
+                    <h1>
+                        Dein Strom- oder Gasvertrag wurde gekündigt?
+                    </h1>
+                    <p>
+                        Du bist unerwartet beim viel teureren Ersatzversorger gelandet? Lass' Dir das nicht gefallen und sichere Dir Deinen Schadensersatz!  Mehr Infos
+                    </p>
+                    <button>Jetzt kostenlos anfragen</button>
                 </div>
+                <img src={banner} alt="banner" />
             </div>
         </div>
     )
